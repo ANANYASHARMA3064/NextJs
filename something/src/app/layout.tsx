1c84d5a1fc55c10eba5bc9dd670344eda17e1c72
Ananya Sharma <ananyasharma3064@gmail.com>
2025-12-20 23:37:43 -0500

    routing

---
+++ something/src/app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "My App",
+  description: "Next.js App Router app",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ something/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home(){
+    return <h1>WELCOME HOME GUDDU PUDDU</h1>
+}
