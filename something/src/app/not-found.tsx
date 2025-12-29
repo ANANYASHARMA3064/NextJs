ea5ba9fd025e9526d37e187c38a6551c5d2a4add
Ananya Sharma <ananyasharma3064@gmail.com>
2025-12-28 21:59:55 -0500

    not-found

---
+++ something/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { usePathname } from "next/navigation"
+export default function NotFound(){
+    const pathname = usePathname();
+    const productID = pathname.split('/')[2]
+    const reviewID = pathname.split('/')[4]
+
+    return(
+        <div>
+            <h2>No page like {reviewID} thissss for {productID}</h2>
+            <p>CHECK!!</p>
+        </div>
+    )
+}
--- something/src/app/products/[productID]/reviews/[reviewID]/page.tsx
+++ something/src/app/products/[productID]/reviews/[reviewID]/page.tsx
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductList({ params, }:{
     params: Promise<{productID:string;reviewID:string}>;
 }){
     const {productID,reviewID} = (await params);
+    if(parseInt(reviewID)>1000){
+        notFound();
+
+    }
     return <h1>Review {reviewID} for product {productID} </h1>
 }
